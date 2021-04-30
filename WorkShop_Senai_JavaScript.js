1438b17ac4152a62e2a00517754afa032a8b878d
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-04-30 09:12:07 -0300

    Números de 0 a 9

--- WorkShop_Senai_JavaScript.js
+++ WorkShop_Senai_JavaScript.js
@@ -1,6 +1,6 @@
-var val1 = Number(prompt("Digite um número"))
+var val1 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
 var sinal = prompt("Informe o tipo de calculo: \n + \n - ")
-var val2 = Number(prompt("Digite outro número"))
+var val2 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
 var resultado = null
 
 function calcular(){
