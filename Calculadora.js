f3219d62c0b85ed1a7449c5ae4214816629f9e2e
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-05-01 20:15:19 -0300

    Segunda inserção

--- Calculadora.js
+++ Calculadora.js
@@ -1,17 +1,9 @@
+var contador=null
 do{
 var val1 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
+contador = contador + val1
 var sinal = prompt("Informe o tipo de calculo: \n + \n - ")
-//var val2 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
-var resultado = null
+contador = contador + sinal
 }while(sinal != "=")
-// // function calcular(){
-// if (sinal == "+") {
-//     resultado = val1 + val2
-// }
-// if (sinal == "-") {
-//     resultado = val1 - val2
-// }
-// return resultado
-// }
-calcular()
-alert(val1 + " " + " " + sinal  +  " " + val2 + " = " + resultado)
+contador
+
