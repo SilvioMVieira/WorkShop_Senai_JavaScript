5072a2c89d58f1c34e9a04617e20205c280fd333
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-05-01 18:27:00 -0300

    Quatro operações

--- WorkShop_Senai_JavaScript.js
+++ WorkShop_Senai_JavaScript.js
@@ -14,3 +14,28 @@ return resultado
 }
 calcular()
 alert(val1 + " " + " " + sinal  +  " " + val2 + " = " + resultado)
+
+//código para as quatro operações
+var val1 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
+var sinal = prompt("Informe o tipo de calculo: \n +  -\n x  / ")
+var val2 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
+var resultado = null
+
+function calcular(){
+if (sinal == "+") {
+    resultado = val1 + val2
+}
+if (sinal == "-") {
+    resultado = val1 - val2
+}
+if (sinal == "x") {
+    resultado = val1 * val2
+}
+if (sinal == "/") {
+    resultado = val1 / val2
+}
+
+return resultado
+}
+calcular()
+alert(val1 + " " + sinal  +  " " + val2 + " = " + resultado)
