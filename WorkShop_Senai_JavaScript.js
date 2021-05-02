3f6258750f2b2b8d81dd60c3c0d7a23b17019576
SilvioMVieira <silvio.vieira@ba.estudante.senai.br>
2021-05-02 18:46:49 -0300

    Novas atualizações

--- WorkShop_Senai_JavaScript.js
+++ WorkShop_Senai_JavaScript.js
@@ -16,9 +16,9 @@ calcular()
 alert(val1 + " " + " " + sinal  +  " " + val2 + " = " + resultado)
 
 //código para as quatro operações
-var val1 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
+var val1 = Number(prompt("Digite um número \n 7   8   9   \n 4   5   6\n 1   2   3\n      0"))
 var sinal = prompt("Informe o tipo de calculo: \n +  -\n x  / ")
-var val2 = Number(prompt("Digite um número \n 0   1   2\n 3   4   5\n 6   7   8\n      9"))
+var val2 = Number(prompt("Digite um número \n 7   8   9   \n 4   5   6\n 1   2   3\n      0"))
 var resultado = null
 
 function calcular(){
